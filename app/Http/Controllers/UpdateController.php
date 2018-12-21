69972851f8e50560eb41a8941fa4de2dabf7f9c6
flatko84 <flatko@gmail.com>
2018-12-21 12:47:25 +0200

    SSE implemented. To do - start / delete / join game logic on home page.

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -6,35 +6,41 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 use Illuminate\Http\Request;
 use App\UserToGame;
 use App\Game;
+use App\Turns;
 use Illuminate\Support\Facades\Auth;
 
 class UpdateController extends Controller {
 
 	public function index($game_id) {
 		$user_id = Auth::id();
-		//date_default_timezone_set("Europe/Sofia");
+		$end = 0;
+		$force = 1;
+		while ($end == 0) {
 
+			$turn = Turns::where('game_id', $game_id)->where('user_id', '!=', $user_id)->where('sent', '0')->first();
 
-		for ($i = 0; $i < 5; $i++) {
-
-
-
-			$game = Game::where('game_id', $game_id)->where('status', '0')->first();
-
-			$user_to_game = UserToGame::where('game_id', $game_id)->where('user_id', '!=', $user_id)->first();
-
-			if ($user_to_game != null) {
+			if ($turn != null) {
+				$force = 0;
+				Turns::where('turn_id', $turn->turn_id)->update(['sent' => '1']);
 				header('Cache-Control: no-cache');
 				header("Content-Type: text/event-stream\n\n");
-				$response['turn'] = unserialize($user_to_game->state);
-				if ($user_to_game->status > -1) {
-					$response['end'] = $user_to_game->status;
+				$response['turn'] = $turn->turn;
+				if ($turn->user_to_game->status > -1) {
+					$response['end'] = $turn->user_to_game->status;
+					$end = 1;
 				}
+				echo "event: turn\n";
+				echo 'data: ' . json_encode($response);
+				echo "\n\n";
+			} elseif ($force == 1) {
+
+				header('Cache-Control: no-cache');
+				header("Content-Type: text/event-stream\n\n");
+				$response['turn'] = "success";
+
 				echo "event: ping\n";
 				echo 'data: ' . json_encode($response);
 				echo "\n\n";
-
-				//echo 'data: This is a message at time ' . $curDate . "\n\n";
 			}
 
 			ob_end_flush();
--- public/js/eventsource.js
+++ public/js/eventsource.js
@@ -1,11 +1,11 @@
 
 var evtSource = new EventSource("/update/" + game_id);
-evtSource.addEventListener("ping", function (e) {
+evtSource.addEventListener("turn", function (e) {
 
     var obj = JSON.parse(e.data);
-    for (i = 0; i < obj['turn'].length; i++) {
-        $('#' + obj['turn'][i]).html(symbol);
-    }
+    console.log(obj['turn']);
+      $('#' + obj['turn']).html(symbol);
+    
 console.log(obj);
     if (obj['end']) {
         switch (obj['end'])
