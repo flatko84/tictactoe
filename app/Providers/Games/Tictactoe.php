775eeae7d93cffd2d5ba37f61a83f03ab45a7f18
flatko84 <flatko@gmail.com>
2019-01-03 15:59:20 +0200

    Trying to implement one controller for all games.

---
+++ app/Providers/Games/Tictactoe.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Providers\Games;
+
+/**
+ * Description of Tictactoe
+ *
+ * @author vdonkov
+ */
+class Tictactoe {
+	private $game_type = "tictactoe";
+	private $game_mode = 2;
+	
+	private $response = array();
+}
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,26 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/tictactoe', 'TictactoeController@index')->name('tictactoe');
-Route::get('/tictactoe/{game_id}','TictactoeController@index');
-Route::get('/pool', 'PoolController@index')->name('tictactoe');
-Route::get('/pool/{game_id}','PoolController@index');
-Route::post('/tictactoe/turn','TictactoeController@turn');
-Route::post('/pool/turn','PoolController@turn');
+Route::get('/game', 'GameController@startGame');
+Route::get('/game/{game_type}/{game_id?}', 'GameController@startGame');
+Route::post('/game/turn', 'GameController@turn');
 
-
-Route::get('/update/{game_id}','UpdateController@index');
-Route::get('/opengames','UpdateController@getOpenGames');
+Route::get('/update/{game_id}', 'UpdateController@index');
+Route::get('/opengames', 'UpdateController@getOpenGames');
